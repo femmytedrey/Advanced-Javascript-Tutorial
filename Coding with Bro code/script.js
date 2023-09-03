2a5fc7a250baa737b61847754d4bf1e609c35e77
femmytedrey <femmytedrey@gmail.com>
2023-09-03 01:51:28 +0100

    exploring new things

--- Coding with Bro code/script.js
+++ Coding with Bro code/script.js
@@ -49,6 +49,41 @@
 
 //     return `${hours}:${minutes}:${seconds} ${amOrPm}`
 // }
+// synchronous code = In an ordered sequence.
+//                                      Step-by-step linear instructions
+//                                     (start now, finish now)
+ 
+// asynchronous code = Out of sequence.
+//                                        Ex. Access a database
+//                                              Fetch a file
+//                                              Tasks that take time
+//                                        (start now, finish sometime later)
+
+// synchronous
+// console.log("START");
+
+// // asynchronous
+// setTimeout(() => console.log("This is asynchronous"), 5000);
+
+// // synchronous
+// console.log("END");
+
+// console.time() = Starts a timer you can use to 
+//                               track how long an operation takes
+//                              Give each timer a unique name.
+
+//start
+// console.time("Response time");
+
+// alert("CLICK THE OK BUTTON!");
+// //setTimeout(() => console.log("HELLO!"), 3000);
+
+// //end
+// console.timeEnd("Response time");
+
+
+
+
 let myLabel = document.querySelector(".date");
 update();
 setInterval(update, 1000);
@@ -59,11 +94,13 @@ myLabel.style.background = "black";
 myLabel.style.alignItems = "center";
 myLabel.style.justifyContent = "center";
 myLabel.style.color = "white";
-myLabel.style.fontSize = "70px";
+myLabel.style.fontSize = "45px";
 myLabel.style.margin = "0px";
 myLabel.style.padding = "0px";
 myLabel.style.boxSizing = "border-box";
 myLabel.style.fontFamily = "Poppins";
+
+
 // const update = (()=>{
 //     let date = new Date;
 //     myLabel.innerHTML = date;
@@ -93,3 +130,51 @@ function update(){
     }
 
 }
+
+
+
+
+// promise = object that encapsulates the result of an asynchronous operation
+//                   let asynchronous methods return values like synchronous methods
+//                   "I promise to return something in the future"
+
+//           the STATE is 'pending' then: 'fulfilled' or 'rejected'
+//           the RESULT is what can be returned
+//           2 parts producing & consuming
+
+// ------------- Example 1 -------------
+// const promise = new Promise((resolve, reject) => {
+ 
+//     let fileLoaded = false;
+ 
+//     if(fileLoaded){
+//         resolve("File loaded");
+//     }
+//     else{
+//         reject("File NOT loaded");
+//     }
+// });
+ 
+// promise.then(value => console.log(value))
+//               .catch(error => console.log(error));
+
+// // ------------- Example 2 -------------
+
+// const wait = time => new Promise(resolve => {
+//     setTimeout(resolve, time);
+// });
+
+// wait(3000).then(() => console.log("Thanks for waiting!"));
+
+//import {PI, getCircumference, getArea} from "./math_util.js";
+import * as MathUtil from "./math_util.js";
+
+console.log(MathUtil.PI);
+
+let circumference = MathUtil.getCircumference(10);
+console.log(circumference);
+
+let area = MathUtil.getArea(10);
+console.log(area);
+
+console.log(document)
